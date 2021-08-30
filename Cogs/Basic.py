b9b59c442e01489f004768b6cb73c5a15ded406b
Chris Scaramella <CScarame@alumni.nd.edu>
2021-08-29 19:42:32 -0700

    found a timer script.  testing...

--- Cogs/Basic.py
+++ Cogs/Basic.py
@@ -33,3 +33,42 @@ class Basic(commands.Cog):
             currentTime = datetime.datetime.today()
             await asyncio.sleep(1)
         await ctx.send("A new minute!")
+
+    @commands.command()
+    async def timer(self, ctx, timeInput):
+        try:
+            try:
+                time = int(timeInput)
+            except:
+                convertTimeList = {'s':1, 'm':60, 'h':3600, 'd':86400, 'S':1, 'M':60, 'H':3600, 'D':86400}
+                time = int(timeInput[:-1]) * convertTimeList[timeInput[-1]]
+            if time > 86400:
+                await ctx.send("I can\'t do timers over a day long")
+                return
+            if time <= 0:
+                await ctx.send("Timers don\'t go into negatives :/")
+                return
+            if time >= 3600:
+                message = await ctx.send(f"Timer: {time//3600} hours {time%3600//60} minutes {time%60} seconds")
+            elif time >= 60:
+                message = await ctx.send(f"Timer: {time//60} minutes {time%60} seconds")
+            elif time < 60:
+                message = await ctx.send(f"Timer: {time} seconds")
+            while True:
+                try:
+                    await asyncio.sleep(5)
+                    time -= 5
+                    if time >= 3600:
+                        await message.edit(content=f"Timer: {time//3600} hours {time %3600//60} minutes {time%60} seconds")
+                    elif time >= 60:
+                        await message.edit(content=f"Timer: {time//60} minutes {time%60} seconds")
+                    elif time < 60:
+                        await message.edit(content=f"Timer: {time} seconds")
+                    if time <= 0:
+                        await message.edit(content="Ended!")
+                        await ctx.send(f"{ctx.author.mention} Your countdown Has ended!")
+                        break
+                except:
+                    break
+        except:
+            await ctx.send(f"Alright, first you gotta let me know how I\'m gonna time **{timeInput}**....")
