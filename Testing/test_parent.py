01da9a25e52f1580707a4d411ffae893d7b0ac10
Chris Scaramella <CScarame@alumni.nd.edu>
2021-08-29 17:10:02 -0700

    bugfix

--- Testing/test_parent.py
+++ Testing/test_parent.py
@@ -7,7 +7,6 @@ def parent(pid):
     
 if __name__ == '__main__':
     currentTime = datetime.datetime.today()
-    addTime = datetime.datetime(minute=1)
     print(str(currentTime))
     print(currentTime)
     parent(os.getpid())
