8b173142ce0a8702aabef12baf5b387c96f8c5f2
Chris Scaramella <CScarame@alumni.nd.edu>
2021-08-29 18:05:00 -0700

    I don't get the format string

--- Testing/test_child.py
+++ Testing/test_child.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     print('Arguments: {}'.format(sys.argv))
     time_str = " ".join(sys.argv[1:])
     print(time_str)
-    goalTime = datetime.datetime.strptime(time_str, '%Y-%m-%d %H:%M:S')
+    goalTime = datetime.datetime.strptime(time_str, 'YYYY-mm-dd HH:MM:SS')
     while True:
         currentTime = datetime.datetime.now()
         if(goalTime < currentTime):
