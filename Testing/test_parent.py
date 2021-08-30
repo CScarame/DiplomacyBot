4f23ac55c6862a60dd00eac192ea55a8bf63c188
Chris Scaramella <CScarame@alumni.nd.edu>
2021-08-29 18:24:56 -0700

    +

--- Testing/test_parent.py
+++ Testing/test_parent.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     futureTime = currentTime.replace(second=0, microsecond=0) + datetime.timedelta(minutes=1)
     print(futureTime)
     parent(os.getpid())
-    subprocess.run(['pwd'])
+    subprocess.check_output("pwd", shell=True, universal_newlines=True)
     child = subprocess.Popen('./test_child.py {}'.format(str(futureTime).split()))
     while child.poll == None:
         print('Parent waiting for child...')
