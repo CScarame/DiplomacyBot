947ab505817ae12c62935179d9e2fa59b718a9d1
Chris Scaramella <CScarame@alumni.nd.edu>
2021-08-29 18:03:37 -0700

    replace doesn't replace, but creates new instance

--- Testing/test_parent.py
+++ Testing/test_parent.py
@@ -7,9 +7,8 @@ def parent(pid):
     print('parent PID: {}'.format(pid))
     
 if __name__ == '__main__':
-    futureTime = datetime.datetime.today()
-    futureTime.replace(second=0, microsecond=0)
-    futureTime = futureTime + datetime.timedelta(minutes=1)
+    currentTime = datetime.datetime.today()
+    futureTime = currentTime.replace(second=0, microsecond=0) + datetime.timedelta(minutes=1)
     print(futureTime)
     parent(os.getpid())
     p = os.system('python ./Testing/test_child.py {}'.format(str(futureTime)))
