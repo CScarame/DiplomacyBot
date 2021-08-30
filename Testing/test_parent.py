a614dad6e65e2ae7f182baa20a599e7b1ff37818
Chris Scaramella <CScarame@alumni.nd.edu>
2021-08-29 18:26:28 -0700

    revert now that I found the real problem

--- Testing/test_parent.py
+++ Testing/test_parent.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     print(futureTime)
     parent(os.getpid())
     subprocess.check_output("pwd", shell=True, universal_newlines=True)
-    child = subprocess.Popen('./test_child.py {}'.format(str(futureTime)).split())
+    child = subprocess.Popen('python3 ./Testing/test_child.py {}'.format(str(futureTime)).split())
     while child.poll == None:
         print('Parent waiting for child...')
         time.sleep(5)
