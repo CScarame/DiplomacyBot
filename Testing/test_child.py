40ebf5306c3910764377d0cfcda9d86e4b5aa14e
Chris Scaramella <CScarame@alumni.nd.edu>
2021-08-29 18:06:13 -0700

    format again

--- Testing/test_child.py
+++ Testing/test_child.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     print('Arguments: {}'.format(sys.argv))
     time_str = " ".join(sys.argv[1:])
     print(time_str)
-    goalTime = datetime.datetime.strptime(time_str, 'YYYY-mm-dd HH:MM:SS')
+    goalTime = datetime.datetime.strptime(time_str, '%Y-%m-%d %H:%M:%S')
     while True:
         currentTime = datetime.datetime.now()
         if(goalTime < currentTime):
